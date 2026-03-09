2545b5e3d3e81040b6f64ea8fe334a0bee3f683e
Hornet5125 <7204653@gmail.com>
2026-03-09 22:16:08 +0300

    oaip

--- oaip/lab1/zad1.cpp
+++ oaip/lab1/zad1.cpp
@@ -4,17 +4,46 @@ int main(){
     int x;
     int inf = 1;
     while (inf == 1){
-        cout << "1) Ввод массива структур" << endl <<
-        "2) Сортировка массива структур" << endl <<
-        "3) Поиск в массиве структур по заданному параметру" << endl <<
-        "4) Изменение заданной структуры" << endl <<
-        "5) Удаление структуры из массива" << endl <<
-        "6) Вывод на экран массива структур" << endl <<
-        "7) Выход." << endl << 
-        "Введите x:";
+        setlocale(LC_ALL, "");
+        cout << "1) Ввод массива структур" << endl; 
+        cout << "2) Сортировка массива структур" << endl;
+        cout << "3) Поиск в массиве структур по заданному параметру" << endl; 
+        cout << "4) Изменение заданной структуры" << endl;
+        cout << "5) Удаление структуры из массива" << endl;
+        cout << "6) Вывод на экран массива структур" << endl;
+        cout << "7) Выход." << endl;
+        cout << "Введите x:";
         cin >> x;
+        system ("cls");
         switch(x){
-            case 1: 
+            case 1:
+                system ("cls");
+                cout << "penis" << endl;
+                break;
+            case 2: 
+                system ("cls");
+                cout << "WW" << endl;
+                break;
+            case 3:
+                system ("cls");
+                cout << "penis" << endl;
+                break;
+            case 4: 
+                system ("cls");
+                cout << "WW" << endl;
+                break;
+            case 5:
+                system ("cls");
+                cout << "penis" << endl;
+                break;
+            case 6: 
+                system ("cls");
+                cout << "WW" << endl;
+                break;
+            case 7: 
+                system ("cls");
+                cout << "WW" << endl;
+                break;
         }
     }
     return 0;
